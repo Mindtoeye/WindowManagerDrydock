87862c92e624d28092322f6a8a752ff0c7920c83
MindtoEye <vvelsen@gmail.com>
2021-11-16 19:54:20 -0500

    Refactored and reworked the build and deploy scripts to work with webpack 5

--- run-install.sh
+++ run-install.sh
@@ -1,1 +1,10 @@
+clear
+
+which curl &> /dev/null
+
+if [[ $? = 0 ]]; then
+  curl https://knossys.com/banner.txt
+fi
+
+export NODE_OPTIONS=--openssl-legacy-provider
 npm install
--- webpack.config.js
+++ webpack.config.js
@@ -4,67 +4,59 @@ const path = require('path');
 const resolve = require('path').resolve;
 
 const config = {
+  mode: 'development',  
   devtool: 'eval-source-map',
-  entry: __dirname + '/js/index.jsx',
+  entry: __dirname + '/src/index.jsx',
   output:{
     path: resolve('./public/'),
     filename: 'bundle.js',
     publicPath: resolve('./public/')
   },
-  // webpack-dev-server configuration
-  devServer: {    // Can be omitted unless you are using 'docker' 
-    host: '0.0.0.0',    // This is where webpack-dev-server serves your bundle which is created in memory.
-    // To use the in-memory bundle,
-    // your <script> 'src' should point to the bundle
-    // prefixed with the 'publicPath', e.g.:
-    //   <script src='http://localhost:9001/assets/bundle.js'>
-    //   </script>
-    publicPath: '/public/',    // The local filesystem directory where static html files
-    // should be placed.
-    // Put your main static html page containing the <script> tag
-    // here to enjoy 'live-reloading'
-    // E.g., if 'contentBase' is '../views', you can
-    // put 'index.html' in '../views/main/index.html', and
-    // it will be available at the url:
-    //   https://localhost:9001/main/index.html
-    contentBase: path.resolve(__dirname, "./views"),    // 'Live-reloading' happens when you make changes to code
-    // dependency pointed to by 'entry' parameter explained earlier.
-    // To make live-reloading happen even when changes are made
-    // to the static html pages in 'contentBase', add 
-    // 'watchContentBase'
-    watchContentBase: true,    
-    compress: true,
-    port: 8081
- },
- plugins: [new HtmlWebpackPlugin({
-  favicon: "./css/images/favicon.ico" 
- })],
- resolve: {
-  extensions: ['.js','.jsx','.css']
- },
- module: {
-  rules: [
-  {
-    test: /\.(js|jsx)$/,
-    exclude: /(node_modules|bower_components)/,
-    loader: 'babel-loader',
-    options: {
-        presets: ['react', 'es2015'],
-        plugins: ['transform-class-properties']
-    }
+  devServer: {
+    static: './public'
   },
-  {
-    test: /\.css$/i,
-    use: ['style-loader', 'css-loader']
+  plugins: [new HtmlWebpackPlugin({
+    template: path.resolve (__dirname,'views/index.html'),
+    filename: 'index.html'      
+  })],
+  resolve: {
+    extensions: ['.js','.jsx','.css','.scss','.png','.jpg']
   },
-  {
-    test: /\.(png|svg|jpe?g|gif)$/i,
-    loader: 'file-loader',
-    options: {
-      publicPath: 'public',
-    }
-  }]
- }
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env','@babel/preset-react']
+        }
+      }
+    },{
+      test: /\.(sa|sc|c)ss$/,
+      use: [
+        // Creates `style` nodes from JS strings
+        "style-loader",
+        // Translates CSS into CommonJS
+        "css-loader",
+        // Compiles Sass to CSS
+        "sass-loader",
+      ],
+    },
+    {
+      test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              esModule: false,
+            },
+          },
+        ],
+
+     type: 'javascript/auto'
+    }]
+  }
 };
 
 module.exports = config;
