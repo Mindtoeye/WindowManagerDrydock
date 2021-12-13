c06e8d3a168796ddef36bb1dc67aefc2df1adc8f
Martin van Velsen <vvelsen@gmail.com>
2021-12-12 23:45:01 -0500

    Lots of small tweaks to the build and deploy scripts. Meeds reworking of the variable usage in the css files

--- run-build.sh
+++ run-build.sh
@@ -7,9 +7,22 @@ if [[ $? = 0 ]]; then
 fi
 
 if [ ! -d "node_modules" ]; then
-  echo "Error: no node_modules folder found, please execute 'run-prep.sh first"
+  echo "Error: no node_modules folder found, please execute 'run-install.sh first"
   exit
 fi
 
-export NODE_OPTIONS=--openssl-legacy-provider
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+echo Building for: ${machine}
+
+if [ "$(machine)" == "Linux" ]; then
+ export NODE_OPTIONS=--openssl-legacy-provider
+fi
+
 npm run build
--- run-install.sh
+++ run-install.sh
@@ -6,5 +6,18 @@ if [[ $? = 0 ]]; then
   curl https://knossys.com/banner.txt
 fi
 
-export NODE_OPTIONS=--openssl-legacy-provider
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+echo Building for: ${machine}
+
+if [ "$(machine)" == "Linux" ]; then
+ export NODE_OPTIONS=--openssl-legacy-provider
+fi
+
 npm install
